02b8f2d36ce5c9e990e85c38f091213289da4f8a
ariunaamy <ariun.myagmar@gmail.com>
2023-01-19 13:10:26 -0500

    select menu, movie description, add reviews, reset reviews, error handling

--- main.js
+++ main.js
@@ -5,62 +5,123 @@ const displayInfo = document.getElementById("display-info");
 const title = document.createElement("h3");
 const releaseYear = document.createElement("p");
 const description = document.createElement("p");
-// To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
+
+const form = document.querySelector("form");
+const ul = document.querySelector("ul");
+const resetButton = document.getElementById("reset-reviews")
+
+const ol = document.querySelector("ol");
 
 function run() {
     fetch(`${baseURL}/films`)
 	.then(response => response.json())
 	.then(result => {
+        const people = 
         createFilmOptions(result);
         movieDetails(result);
+        addReview(result);
     })
-	.catch(err => console.error(err));
+        .catch(err => console.error(err));
 
-
- // Add code you want to run on page load here
+        fetch(`${baseURL}/people`)
+        .then(response => response.json())
+        .then(data => {
+            showPeople(data);
+        })
+        .catch(err => console.error(err));
 }
 
-// This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
-// So that testing can work as expected for now
-// A non-hacky solution is being researched
+
 
 setTimeout(run, 1000);
 
-
-
-
-// option elements should have a value of the movie id and the text should be the movie title.
-// Movie div id=display-info
-// h3(movie title)
-// p (moves release year)
-// p (description of the movie)
-
-
-
-
 function createFilmOptions(result) {
+
     for (let i=0; i<result.length; i++){
     const option = document.createElement("option");
     selectTitles.append(option);
 
     option.innerHTML=result[i].title;
     option.setAttribute("value", result[i].id);
+
     }
 }
 
 function movieDetails(result){
- selectTitles.addEventListener("click",()=>{
-   
-   displayInfo.append(title);
-   displayInfo.append(releaseYear);
-   displayInfo.append(description);
+    let count = 0;
+    selectTitles.addEventListener("change", (event) => {
+        count++;
+        displayInfo.append(title);
+        displayInfo.append(releaseYear);
+        displayInfo.append(description);
 
-   for (let i=0; i<result.length; i++){
-    if (selectTitles.value === result[i].id){
-        title.innerHTML = result[i].title;
-        releaseYear.innerHTML = result[i].release_date;
-        description.innerHTML = result[i].description;
+
+        for (let i = 0; i < result.length; i++) {
+            if (selectTitles.value === result[i].id) {
+                title.innerHTML = result[i].title;
+                releaseYear.innerHTML = result[i].release_date;
+         // releaseYear.setAttribute("value", result[i].release_date);
+             description.innerHTML = result[i].description;
+            }
+        }
+
+        if (count > 1) {
+            ol.innerHTML = "";
+            document.getElementById("show-people").disabled = false;
     }
-   }
-  })
+    })
+
 }
+
+function addReview(result) {
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        if (selectTitles.selectedIndex === 0) {
+            alert("Please select a movie first")
+        }
+        else {
+            const li = document.createElement("li");
+            ul.append(li);
+
+            let title = "<strong><b>" + selectTitles[selectTitles.selectedIndex].innerHTML + "</b></strong>";
+            li.innerHTML = title + ": " + document.getElementById("review").value;
+            form.reset();
+
+            resetButton.addEventListener("click", () => {
+                li.remove();
+            })
+        }
+    })
+}
+
+function showPeople(data) {
+    let count = 0;
+    document.getElementById("show-people").addEventListener("click", () => {
+        count++
+        if (count > 1) {
+             document.getElementById("show-people").disabled = true;
+        }
+        else {
+            for (let i = 0; i < data.length; i++) {
+                let filmID = data[i].films[0].slice(7);
+                if (selectTitles.value === filmID) {
+                    const li = document.createElement("li");
+                    ol.append(li);
+                    li.innerHTML = data[i].name;
+                }
+            }
+
+        }
+    })
+}
+
+
+// if (ol.length>0){
+//    
+//     }  
+
+
+
+
+
